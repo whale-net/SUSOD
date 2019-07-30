ae9502488c00f9116e8709a89bc128aef1c87944
qmastermoo <alex.harding@whale-net.net>
2019-07-29 21:31:59 -0400

    added basic login decorator

--- SUSOD/views/index.py
+++ SUSOD/views/index.py
@@ -5,12 +5,13 @@ Used to make sure flask app runs.
 """
 import flask
 import SUSOD
-from SUSOD.util import *
+from SUSOD.util import get_login_context, has_permissions
 
 # TODO: move to index module
 # this is here for testing right now
 
 @SUSOD.app.route('/')
+@has_permissions
 def show_index():
 	""" Display /example page."""
 
